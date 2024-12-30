ba75944bf6f181ec56666fcc03afda6ed0d98f5d
davitti <abuadonald@gmail.com>
2024-12-30 18:28:06 +0100

    Add task model with CRUD operations using Prisma.

---
+++ src/model/tasks.model.js
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function getTasks() {
+	return await prisma.task.findMany();
+}
+
+export async function createTask(data) {
+	return await prisma.task.create({
+		data,
+	});
+}
+
+export async function updateTask(id, data) {
+	return await prisma.task.update({
+		data,
+		where: { id },
+	});
+}
+
+export async function deleteTask(id) {
+	return await prisma.task.delete({
+		where: { id },
+	});
+}
